bd926097c11c9aa78d28750b359b6c89592bec64
Eduardo Spat Schwingel <esschwingel@inf.ufsm.br>
2025-05-13 04:16:41 -0300

    melhora geral
    limpa no header
    menu no banner inicial
    redes e contato no fim da pagina inicial

--- site_inf/static/site_inf/js/slider.js
+++ site_inf/static/site_inf/js/slider.js
@@ -1,40 +1,53 @@
 let slider = document.querySelector('.slider .list');
 let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
 let dots = document.querySelectorAll('.slider .dots li');
+let pauseButton = document.getElementById('pause');
 
 let lengthItems = items.length - 1;
 let active = 0;
-next.onclick = function(){
+let isPaused = false;
+
+function nextSlide() {
     active = active + 1 <= lengthItems ? active + 1 : 0;
     reloadSlider();
 }
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active - 1 : lengthItems;
-    reloadSlider();
-}
-let refreshInterval = setInterval(()=> {next.click()}, 3000);
-function reloadSlider(){
+
+function reloadSlider() {
     slider.style.left = -items[active].offsetLeft + 'px';
-    // 
+
     let last_active_dot = document.querySelector('.slider .dots li.active');
-    last_active_dot.classList.remove('active');
+    if (last_active_dot) last_active_dot.classList.remove('active');
     dots[active].classList.add('active');
-
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 3000);
-
-    
 }
 
-dots.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         active = key;
-         reloadSlider();
-    })
-})
-window.onresize = function(event) {
-    reloadSlider();
+// Avanço automático
+let refreshInterval = setInterval(nextSlide, 3000);
+
+pauseButton.onclick = function () {
+    const icon = pauseButton.querySelector('i');
+
+    if (isPaused) {
+        refreshInterval = setInterval(nextSlide, 3000);
+        icon.classList.remove('fa-play');
+        icon.classList.add('fa-pause');
+    } else {
+        clearInterval(refreshInterval);
+        icon.classList.remove('fa-pause');
+        icon.classList.add('fa-play');
+    }
+
+    isPaused = !isPaused;
 };
 
+// Clique nas bolinhas
+dots.forEach((li, key) => {
+    li.addEventListener('click', () => {
+        active = key;
+        reloadSlider();
+    });
+});
+
+// Reajuste no resize
+window.onresize = function () {
+    reloadSlider();
+};
